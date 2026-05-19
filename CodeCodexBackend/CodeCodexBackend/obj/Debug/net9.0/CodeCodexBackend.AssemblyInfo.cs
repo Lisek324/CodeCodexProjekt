a8dfe5246ab6be3d75a474e62d7872ae975e3c26
Jakub Brzozowski <brzozowski2004@gmail.com>
2026-05-19 14:59:44 +0200

    refactor: change to dockerfile config & removed secrets

--- CodeCodexBackend/CodeCodexBackend/obj/Debug/net9.0/CodeCodexBackend.AssemblyInfo.cs
+++ CodeCodexBackend/CodeCodexBackend/obj/Debug/net9.0/CodeCodexBackend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodeCodexBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d28a05b39d8192dab2c3c4c4c06a8e961a7be1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcce905b100837626228fa5285c4599df3933f78")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodeCodexBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodeCodexBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
