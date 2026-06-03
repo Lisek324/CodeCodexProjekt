253553a839cacd1ef09a50a5a89a0570bcd30b68
Jakub Brzozowski <brzozowski2004@gmail.com>
2026-06-03 15:12:08 +0200

    fix: Change paths in environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:false,
-    apiUrl: 'https://localhost:7110/api/',
+    apiUrl: 'https://codecodexprojekt.onrender.com/api/',
     clientId: "337935496962-0uknn98jqpefseflb61oo4r95rdv66j2.apps.googleusercontent.com"
 };
